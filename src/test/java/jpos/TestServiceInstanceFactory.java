dac7f4439b086c15cfd908000a0c6f326fcc4b19
Denis Kuniß <denis.kuniss@dieboldnixdorf.com>
2020-04-02 18:36:02 +0200

    Corrected license header.

--- src/test/java/jpos/TestServiceInstanceFactory.java
+++ src/test/java/jpos/TestServiceInstanceFactory.java
@@ -1,18 +1,23 @@
-package jpos;
-
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 //
+// The JavaPOS library source code is under the CPL license, which 
+// is an OSS Apache-like license. The complete license is located at:
+//    http://www.ibm.com/developerworks/library/os-cpl.html
+//
+//------------------------------------------------------------------------------
 // This software is provided "AS IS".  The JavaPOS working group (including
-// each of the Corporate members, contributors and individals)  MAKES NO
-// REPRESENTATIONS OR WARRRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE,
-// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
-// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// each of the Corporate members, contributors and individuals)  MAKES NO
+// REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 // NON-INFRINGEMENT. The JavaPOS working group shall not be liable for
 // any damages suffered as a result of using, modifying or distributing this
-// software or its derivatives. Permission to use, copy, modify, and distribute
-// the software and its documentation for any purpose is hereby granted. 
-//
-///////////////////////////////////////////////////////////////////////////////
+// software or its derivatives.Permission to use, copy, modify, and distribute
+// the software and its documentation for any purpose is hereby granted.
+////////////////////////////////////////////////////////////////////////////////
+
+package jpos;
+
 import java.lang.reflect.Constructor;
 
 import jpos.config.JposEntry;
